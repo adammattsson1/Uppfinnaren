a25c564ac6265e7818fa80794b2ddaa1b5c6248a
adammattsson1 <adammattsson92@gmail.com>
2024-12-12 16:46:45 +0100

    init

--- obj/Debug/net8.0/Uppfinnaren.AssemblyInfo.cs
+++ obj/Debug/net8.0/Uppfinnaren.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uppfinnaren")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830b1e7db5f27f98023848f3cae56938134cd4d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uppfinnaren")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uppfinnaren")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
